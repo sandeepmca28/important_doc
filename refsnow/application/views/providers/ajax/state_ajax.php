af1dc50b86034a5d4c5b9b2e8ac72e1eff27edfc
Sandeep Thakur <sandeepthakur@mobikasa.com>
2016-01-14 10:27:47 +0530

    14-jan 2016

--- refsnow/application/views/providers/ajax/state_ajax.php
+++
@@ -1,9 +0,0 @@
-<option value=""> Select State </option>
-<?php 
-foreach($states as $key => $value)
-{
-  ?>
-    <option <?php if($stateid == $value->id){ echo "selected"; } ?> value="<?php echo $value->id; ?>"><?php echo $value->name; ?></option>
-  <?php
-}
-?>
